ae12aea890cb13eec820c8646a73f6de8bbe16f5
EmnaWalha99 <ewalha12@gmail.com>
2025-07-17 15:56:56 +0100

    prometheus & grafana

--- main.py
+++ main.py
@@ -2,33 +2,57 @@ from fastapi import FastAPI
 from extractors.feature_extractor import FeatureExtractor
 from rules.rule_engine import RuleEngine
 from dataAccess.logging import log_transaction
+from time import time 
+from metrics import (
+  REQUEST_COUNT,
+  EXCEPTION_COUNT,
+  REQUEST_DURATION,
+  TRANSACTION_COUNT,
+  FEATURE_EXTRACTION_TIME
+)
+
+from prometheus_client import make_asgi_app
 
 app = FastAPI(title="Key Watch")
+
+metrics_app = make_asgi_app()
+app.mount("/metrics" , metrics_app)
+
 extractor = FeatureExtractor()
 rules_engine = RuleEngine("rules/rules.yaml")
 @app.post("/extract-features")
 def extract_features(transaction_data : dict):
-  
+  REQUEST_COUNT.inc()
+  start= time()
   try : 
-    features = extractor.extract_features(transaction_data)
+    with FEATURE_EXTRACTION_TIME.time():
+      features = extractor.extract_features(transaction_data)
     return {
       "status" : "data extracted successfully",
       "features" : features
     }
   except Exception as e : 
+    EXCEPTION_COUNT.inc()
     return { 
           "status" :"error" ,
           "message" : str(e)
         }
+  finally :
+    REQUEST_DURATION.observe(time()-start)
     
 @app.post("/apply-rules")
 def apply_rules(transaction_data: dict):
+    REQUEST_COUNT.inc()
+    start = time()
     try:
-        features = extractor.extract_features(transaction_data)
+        with FEATURE_EXTRACTION_TIME.time():
+          features = extractor.extract_features(transaction_data)
         result = rules_engine.evaluate(features)
-        
+        TRANSACTION_COUNT.inc()
+
         #loggin the transactions 
         log_transaction(transaction_data, features, result)
+        TRANSACTION_COUNT.inc()
 
         return {
             "status": "evaluating data successfully",
@@ -36,10 +60,13 @@ def apply_rules(transaction_data: dict):
             "fraud_result": result
         }
     except Exception as e:
+        EXCEPTION_COUNT.inc()
         return {
             "status": "error",
             "message": str(e)
         }
+    finally:
+      REQUEST_DURATION.observe(time()-start)
 @app.get("/")
 def root():
     return {"message": "Fraud Detection API - Ready!"}
--- metrics.py
+++ metrics.py
@@ -1,4 +1,17 @@
-from prometheus_client import Counter, Histogram, Summary
+from prometheus_client import Counter, Histogram, Summary , Gauge
+import psutil
+import time 
+from threading import Thread
+import threading
+
+#cpu and memory usage 
+CPU_USAGE= Gauge("system_cpu_usage_percent","CPU usage percentage")
+MEMORY_USAGE= Gauge("system_memory_usage_percent","Memory usage percentage")
+UPTIME = Gauge("uptime_seconds", "Uptime of the service in seconds")
+THREAD_COUNT = Gauge("system_thread_count", "Number of active threads")
+DISK_USAGE = Gauge("system_disk_usage_percent", "Disk usage percentage")
+PROCESS_RSS_MB = Gauge("system_process_rss_mb", "Memory usage (RSS) of this process in MB")
+
 
 #system metrics
 REQUEST_COUNT = Counter("request_count", "Total number of HTTP requests")
@@ -9,3 +22,22 @@ REQUEST_DURATION= Histogram("request_duration_seconds","Histogram of request dur
 TRANSACTION_COUNT = Counter("transaction_total","Total number of transactions processed")
 FEATURE_EXTRACTION_TIME=Summary("feature_extraction_duration_seconds","Time taken to extract features")
 
+
+#internal timer
+START_TIME=time.time()
+
+def monitor_system_metrics():
+    while True:
+        try:
+            CPU_USAGE.set(psutil.cpu_percent(interval=1))
+            MEMORY_USAGE.set(psutil.virtual_memory().percent)
+            UPTIME.set(time.time()- START_TIME)
+            THREAD_COUNT.set(threading.active_count())
+            DISK_USAGE.set(psutil.disk_usage('/').percent)
+            PROCESS_RSS_MB.set(psutil.Process().memory_info().rss / 1024 / 1024)
+        except Exception as e : 
+            print(f"[Monitoring Error] {e}")
+        time.sleep(5)
+        
+# start background monitoring
+Thread(target=monitor_system_metrics , daemon=True).start()
